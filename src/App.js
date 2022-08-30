ce0e55f1bcad7f8e2bcd5431be9d82b50e134276
tmoropou <63266709+tmoropou@users.noreply.github.com>
2022-08-29 19:57:11 -0700

    Adjusting main theme

--- src/App.js
+++ src/App.js
@@ -2,18 +2,39 @@ import logo from './logo.svg';
 import React from 'react'
 import ReactPlayer from 'react-player'
 import ReactAudioPlayer from 'react-audio-player';
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 import MainPage from './components/MainPage'
 import './App.css';
+import { ThemeProvider } from '@emotion/react';
+
+const theme = extendTheme({
+  styles: {
+    global: {
+      // styles for the `body`
+      body: {
+        bg: '#1d2542',
+        color: '',
+      },
+      // styles for the `a`
+      a: {
+        color: 'red',
+        _hover: {
+          textDecoration: 'underline',
+        },
+      },
+    },
+  },
+})
+
+
 
 function App() {
   return (
+      <ChakraProvider theme={theme}>
+        <MainPage>
 
-    <ChakraProvider>
-      <MainPage>
-
-      </MainPage>
-    </ChakraProvider>
+        </MainPage>
+      </ChakraProvider>
   );
 }
 
--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -89,7 +89,7 @@ function MainPage() {
             </Center>
 
             <div id={'Music'}>
-                <Box mt={'445'} pb={'300'}>
+                <Box mt={'445'} pb={'0'}>
                     <MusicPage />
                 </Box>
             </div>
